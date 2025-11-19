256e7d8a26bf5a5fa1d62b88387e3f9f7c0dc3fe
shilpacoderai <shilpapujer0@gmail.com>
2025-11-19 13:08:24 +0530

    Create 1 D Array2.c

---
+++ 1 D Array2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int rose[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", rose[i]);   // prints the values
+    }
+
+    return 0;
+}
