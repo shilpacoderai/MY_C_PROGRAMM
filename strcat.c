d9fe2bef6ac3885ab4f66c0452fed9b9c1471dc8
shilpacoderai <shilpapujer0@gmail.com>
2025-11-20 10:47:16 +0530

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+int main(){
+char lkp[10];
+    
+        printf("enter your string\n");
+        scanf("%s", lkp);
+        printf("before sting  %s\n", lkp);
+        strcat(lkp,"bgk");
+        printf("after string  %s\n", lkp);
+    }
